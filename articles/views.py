2e4a14ea10a1a5670cc1632ef3de7f0352f896d0
raulgane <omar.elbanaoui007@gmail.com>
2021-11-16 14:02:39 +0100

    add create.html

--- articles/views.py
+++ articles/views.py
@@ -5,12 +5,10 @@ from .models import Article
 def article_search_view(request):
     query_dict = request.GET # this is a dictionary
     # query = query_dict.get('q') # base.html <input type='text' name='q' />
-
     try:
         query = int(query_dict.get('q'))
     except:
         query = None
-
     article_obj = None
     if query is not None:
         article_obj = Article.objects.get(id=query)
@@ -20,10 +18,18 @@ def article_search_view(request):
     return render(request, 'articles/search.html', context=context)
 
 
+
+def article_create_view(request):
+    print(request.POST)
+    context = {}
+    return render(request, 'articles/create.html', context=context)
+
+
+
+
 def article_details_view(request, id=None):
 
     article_obj = None
-
     if id is not None:
         article_obj = Article.objects.get(id=id)
 
