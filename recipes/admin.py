a4448fee476b059858b06e606aa60aeaaf194add
raulgane <omar.elbanaoui007@gmail.com>
2021-11-24 19:21:28 +0100

    24/11/2021 new stuff

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,20 +3,17 @@ from django.contrib.auth import get_user_model
 
 from .models import Recipe, RecipeIngredient
 
-User = get_user_model
+User = get_user_model()
 
-admin.site.register(RecipeIngredient)
-
-class UserInline(admin.TabularInline):
-    model = User
 
 class RecipeIngredientInline(admin.StackedInline):
     model = RecipeIngredient
     extra = 0
+    readonly_fields = ['quantity_as_float', 'as_mks', 'as_imperial']
     # fields = ['name', 'quantity', 'unit', 'directions']
 
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [UserInline, RecipeIngredientInline]
+    inlines = [RecipeIngredientInline]
     list_display = ['name', 'user']
     readonly_fields = ['timestamp', 'updated']
     raw_id_fields = ['user']
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Recipe, RecipeIngredient
+from .admin import User
 
-# Create your tests here.
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user('raulgane', password='123321')
+        self.recipe_a = Recipe.objects.create(
+            name = 'Tacos',
+            user = self.user_a
+        )
+        self.recipe_b = Recipe.objects.create(
+            name = 'Grilled Chicken',
+            user = self.user_a
+        )
+        self.recipe_ingredient_a = RecipeIngredient.objects.create(
+            recipe = self.recipe_a,
+            name = 'Grilled Chicken',
+            quantity = '2 1/2',
+            unit = 'pound'
+        )
+        self.recipe_ingredient_b = RecipeIngredient.objects.create(
+            recipe = self.recipe_b,
+            name = 'seafood salad',
+            quantity = 'abc',
+            unit = 'kg'
+        )
+
+    def test_quantity_as_float(self):
+        self.assertIsNotNone(self.recipe_ingredient_a.quantity_as_float)
+        self.assertIsNone(self.recipe_ingredient_b.quantity_as_float)
